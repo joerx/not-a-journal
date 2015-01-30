3dc94832735a3ed35930e38897cafc8b3855883c
Joerg Henning <henning.joerg@gmail.com>
2015-01-30 22:17:03 +0700

    Angular module, controller, form validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,35 @@
+(function() {
 
+  var app = angular.module('NotAJournalApp', []);
+
+  /**
+   * Controller to create new entries
+   * @param $scope
+   */
+  app.controller('EntryFormController', function($scope, $interpolate) {
+    $scope.entry = {
+      title: '',
+      content: '',
+      author: {
+        name: '',
+        email: ''
+      }
+    };
+    $scope.submitEntry = function() {
+      if (!$scope.entryForm.$valid) {
+        console.warn('Form not valid, cowardly refusing to submit');
+      } else {
+        console.log($scope.entry);
+      }
+    };
+  });
+
+  /**
+   * Controller for the list of entries
+   * @param $scope
+   */
+  app.controller('EntryListController', function($scope) {
+
+  });
+
+})();
