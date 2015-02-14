97a722e5badd90d029f209e930a9771b7dc78445
Joerg Henning <henning.joerg@gmail.com>
2015-02-14 19:44:38 +0700

    Frontend router, edit entry

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var services = angular.module('NotAJournalApp.Services', []);
+  var services = angular.module('notAJournal.Services', []);
 
   /**
    * Service to retrieve and manipulate journal entries from the backend.
@@ -28,13 +28,36 @@
     }
   });
 
-  var app = angular.module('NotAJournalApp', ['NotAJournalApp.Services']);
+  var app = angular.module('notAJournal', [
+    'ngRoute',
+    'notAJournal.Services'
+  ]);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/entry-list.html',
+        controller: 'EntryListController'
+      })
+      .when('/entry/new', {
+        templateUrl: 'views/entry-form.html',
+        controller: 'EntryFormController'
+      })
+      .when('/entry/edit/:id', {
+        templateUrl: 'views/entry-form.html',
+        controller: 'EntryFormController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  });
 
   /**
    * Controller to create new entries
    * @param $scope
    */
-  app.controller('EntryFormController', function($scope, Entries) {
+  app.controller('EntryFormController', function($scope, $location, $routeParams, Entries) {
+
     $scope.entry = {
       title: 'Test Entry',
       content: 'Lorem ipsum dolor sit amet',
@@ -43,20 +66,46 @@
         email: 'johndoe@acme.org'
       }
     };
+
+    if ($routeParams.id) {
+      $scope.id = $routeParams.id;
+      loadEntry();
+    }
+
+    function loadEntry() {
+      Entries.get($scope.id)
+        .then(function(response) {
+          $scope.entry = response.data.data;
+        })
+        .catch(function(response) {
+          console.error('error retrieving entry');
+        });
+    }
+
+    function postEntry() {
+      return Entries.post($scope.entry);
+    }
+
+    function putEntry() {
+      return Entries.put($scope.id, $scope.entry);
+    }
+
     $scope.submitEntry = function() {
       if (!$scope.entryForm.$valid) {
         console.warn('Form not valid, cowardly refusing to submit');
       } else {
-        Entries.post($scope.entry)
+        ($scope.id ? putEntry() : postEntry())
           .then(function(response) {
             console.log('success!', response);
             $scope.$emit('entry.added', response.data.data);
+            $location.url('/');
           })
           .catch(function(response) {
-            console.error('error creating entry', response);
+            console.error('error saving entry', response);
           });
       }
     };
+
   });
 
   /**
@@ -66,11 +115,6 @@
   app.controller('EntryListController', function($scope, $rootScope, Entries) {
     $scope.entries = [];
     $scope.loading = false;
-
-    $rootScope.$on('entry.added', function(evt, entry) {
-      console.log(entry);
-      $scope.entries.push(entry);
-    });
 
     (function loadData() {
       $scope.loading = true;
